34b315039294a0177e0784f6ee0658e19bc34168
mlh-localhost <waynemm14@gmail.com>
2022-05-29 10:57:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+var config1;
+var config2;
+
+fetch("config.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        
+        config1 = data.periodContaining
+        config2 = data.sentenceEnding
+        console.log(config1)
+        console.log(config2)
+    }
+        
+    )
 // ******* index.js *******
 //EASY WAY
 //var myName = window.prompt("What's your name?");
@@ -10,6 +25,32 @@ document.getElementById("myButton").onclick = function(){
     var myName = document.getElementById("myText").value;
     console.log("Hello",myName);
 }*/
+
+function periodCheck(word){
+    for(var c in config1){
+
+        if(word.includes(config1[c])){
+            //console.log("true");
+            return true;
+            
+        }
+    }
+    //console.log("false");
+    return false;
+    
+}
+
+function sentenceCheck(word){
+    for(var c in config2){
+        if(word.includes(config2[c])){
+            //console.log("true");
+            return true;
+            
+        }
+    }
+    return false;
+}
+
 function shuffleArray(array) {
     let curId = array.length;
     // There remain elements to shuffle
@@ -34,6 +75,8 @@ function myFunction(){
 
 document.getElementById("myButton").onclick = function(){
 
+
+    
     var str = document.getElementById("myText").value;
     console.log(str);
     str = str.replaceAll("\n", " ") + " ";
@@ -41,27 +84,36 @@ document.getElementById("myButton").onclick = function(){
     var word = "";
     var num = 1;
     var final = "";
+    
     for(let i = 0; i < str.length; ++i){
+        
         if(str[i] == ' '){
             lyst.push(word);
-            if(word[word.length - 1] == '.' || word[word.length - 1] == '!' ||word[word.length - 1] == '?' ||word[word.length - 1] == '"' ){
-                lyst = shuffleArray(lyst);
-                final += num + ". ";
-                ++num;
-                for(let j = 0; j < lyst.length; ++j){
-                    lyst[j] = lyst[j].replaceAll("+", " ");
-                }
-
-                for(let j = 0; j < lyst.length - 1; ++j){
-                    final += lyst[j] + " / ";
-                }
-                final += lyst[lyst.length - 1] + "\n\n";
-                lyst = [];
+            
+            if(sentenceCheck(word) && !periodCheck(word)){
+                console.log(word);
+                
+                    lyst = shuffleArray(lyst);
+                    final += num + ". ";
+                    ++num;
+                    for(let j = 0; j < lyst.length; ++j){
+                        lyst[j] = lyst[j].replaceAll("+", " ");
+                    }
+    
+                    for(let j = 0; j < lyst.length - 1; ++j){
+                        final += lyst[j] + " / ";
+                    }
+                    final += lyst[lyst.length - 1] + "\n\n";
+                    lyst = [];
+                    
                 
             }
+            
+            
             word = "";
         }else{
             word += str[i];
+            
         }
     }
 
